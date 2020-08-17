30195fca563c80b91cf5c83fee7f99894f940a24
IvanCampos11 <39417756+IvanCampos11@users.noreply.github.com>
2020-08-17 18:01:54 -0400

    Finished assignment

--- twitoff/models.py
+++ twitoff/models.py
@@ -28,7 +28,25 @@ class Tweet(DB.Model):
 def insert_example_users():
     """Example data to play with."""
     austen = User(id=1, name='austen')
+    austen_tweet = Tweet(id=1, text='Lambda is cool!', user_id=1)
     elon = User(id=2, name='elonmusk')
+    elon_tweet = Tweet(id=2, text='SpaceX is cool!', user_id=2)
+    elon_tweet2 = Tweet(
+        id=6, text='The Model S will now come with ejection seats.', user_id=2)
+    fred = User(id=3, name='fred')
+    fred_tweet = Tweet(id=3, text='SpaceX and Lambda are cool!', user_id=3)
+    fred_tweet2 = Tweet(
+        id=4, text='How much wood would a woodchuck chuck?', user_id=3)
+    fred_tweet3 = Tweet(
+        id=5, text='The meaning of life and everything is 42.', user_id=3)
     DB.session.add(austen)
     DB.session.add(elon)
+    DB.session.add(fred)
+    DB.session.commit()
+    DB.session.add(austen_tweet)
+    DB.session.add(fred_tweet)
+    DB.session.add(fred_tweet2)
+    DB.session.add(fred_tweet3)
+    DB.session.add(elon_tweet)
+    DB.session.add(elon_tweet2)
     DB.session.commit()
